38cfc31974bc283ad20305453cfd35b3f67841c8
Ludmila L <millarissa.valdes@gmail.com>
2018-11-26 18:40:52 +0200

    Create AskQuestion module

---
+++ app/code/Ludmila/LSAskQuestion/view/frontend/requirejs-config.js
@@ -0,0 +1,17 @@
+var config = {
+    map: {
+        '*': {
+            ask_question: 'Ludmila_LSAskQuestion/js/ask_question'
+        }
+    }
+    // config: {
+    //     mixins: {
+    //         'Magento_Catalog/js/catalog-add-to-cart': {
+    //             'Geekhub_RequestSample/js/product/catalog-add-to-cart-mixin': true
+    //         },
+    //         'Magento_Checkout/js/action/place-order': {
+    //             'Geekhub_RequestSample/js/checkout/place-order-mixin': true
+    //         },
+    //     }
+    // }
+};
