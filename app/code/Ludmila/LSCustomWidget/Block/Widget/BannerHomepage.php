48328206fc41251a335d4cc284ac83285247cda8
Ludmila L <millarissa.valdes@gmail.com>
2018-12-19 18:48:13 +0200

    Create homepage banner

---
+++ app/code/Ludmila/LSCustomWidget/Block/Widget/BannerHomepage.php
@@ -0,0 +1,7 @@
+<?php
+namespace Ludmila\LSCustomWidget\Block\Widget;
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+class BannerHomepage extends Template implements BlockInterface {
+    protected $_template = "new_banner_homepage.phtml";
+}
