4a703dd73495e316ed2fe26cc2b8a3180368c06c
Ludmila L <millarissa.valdes@gmail.com>
2019-03-26 21:40:58 +0200

    HW_18 Show products list

--- app/code/Ludmila/LSCustomerOrder/Block/Onepage.php
+++ app/code/Ludmila/LSCustomerOrder/Block/Onepage.php
@@ -6,6 +6,9 @@ class Onepage extends \Magento\Framework\View\Element\Template
     public function getJsLayout()
     {
         $this->jsLayout['components']['onepageScope']['children']['steps']['children']['customer-step']['config']['customersListUrl'] = $this->getUrl('customer/customer/getList');
+
+        $this->jsLayout['components']['onepageScope']['children']['steps']['children']['product-step']['config']['productsListUrl'] = $this->getUrl('customer/product/getList');
+
         return json_encode($this->jsLayout, JSON_HEX_TAG);
     }
 }
--- app/code/Ludmila/LSCustomerOrder/view/frontend/web/js/view/product.js
+++ app/code/Ludmila/LSCustomerOrder/view/frontend/web/js/view/product.js
@@ -4,19 +4,29 @@ define([
     'uiComponent',
     'ko',
     'Magento_Checkout/js/model/step-navigator',
-    'mage/translate'
+    'mage/translate',
+    'mageUtils',
+    'Ludmila_LSCustomerOrder/js/model/product-service'
 ], function (
     $,
     _,
     Component,
     ko,
     stepNavigator,
-    $t
+    $t,
+    utils,
+    productService
 ) {
     'use strict';
 
     return Component.extend({
-        defaults: {},
+        defaults: {
+            customers: [],
+            listens: {
+                responseData: 'updateProductsList',
+                request: 'searchRequest'
+            }
+        },
         isVisible: ko.observable(false),
 
         /** @inheritdoc */
@@ -31,7 +41,48 @@ define([
                 20
             );
 
+            var self = this;
+            this.initProductList();
+            // this.responseData.subscribe(function(data){
+            //     self.customers(data.customers);
+            // });
             return this;
+        },
+
+        initObservable: function () {
+            return this._super()
+                .observe([
+                    'responseData',
+                    'responseStatus',
+                    'products',
+                    'request'
+                ]);
+        },
+
+        initProductList: function (params) {
+            productService.getProductList(
+                params,
+                {
+                    url: this.productsListUrl
+                },
+                {
+                    data: this.responseData,
+                    status: this.responseStatus
+                }
+            );
+        },
+
+        updateProductsList: function (data) {
+            this.products(data.products);
+        },
+
+
+        chooseProduct: function (product) {
+            alert(product.sku);
+        },
+
+        searchRequest: function (request) {
+            this.initProductList({q:request});
         },
 
         /**
